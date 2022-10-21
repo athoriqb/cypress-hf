34ccbeb02d4bbb6598638a1aa7a2a0589c4de979
athoriqb <athoriqb@gmail.com>
2022-10-22 05:39:09 +0700

    test

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,5 +1,27 @@
-describe("My First Test suite", function() {
-    it('My First Test Case', function() {
-        
-    });
-})
+describe("My First Test suite", () => {
+    it('My First Test Case', () => {
+      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+      cy.get('.search-keyword').type("ca")
+      cy.wait(2000)
+      // get element with attribute
+      cy.get(".product:visible").should('have.length', 4)
+      // get element start from parent to descendent
+      cy.get(".products").find(".product").should('have.length', 4)
+      // get element from index array
+      cy.get(".products")
+        .find(".product")
+        .eq(1).contains('CART')
+        .click()
+        .should('contain.text', 'ADDED')
+      // looping  
+      cy.get(".products")
+        .find(".product")
+        .each(($el, index, $list) => {
+
+          const productName=$el.find("h4.product-name").text()
+          if (productName.includes("Cashews")) {
+            cy.wrap($el).find("button").click()
+          }
+        })
+    })
+  })
