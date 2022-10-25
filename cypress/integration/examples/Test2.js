cbf2eee62319e4798ffd6853447a0057b8749111
athoriqb <athoriqb@gmail.com>
2022-10-25 08:37:17 +0700

    course 5

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -5,16 +5,20 @@ describe("My First Test suite", () => {
       cy.wait(2000)
       // get element with attribute
       cy.get(".product:visible").should('have.length', 4)
+      // Parent child chining
+      cy.get(".products").as("productLocator")
       // get element start from parent to descendent
-      cy.get(".products").find(".product").should('have.length', 4)
+      cy.get("@productLocator").find(".product").should('have.length', 4)
       // get element from index array
-      cy.get(".products")
+      cy.get("@productLocator")
         .find(".product")
         .eq(1).contains('CART')
         .click()
-        .should('contain.text', 'ADDED')
+        .should('contain.text', 'ADDED').then(() => {
+          console.log("tf")
+        })
       // looping  
-      cy.get(".products")
+      cy.get("@productLocator")
         .find(".product")
         .each(function ($el) {
           
@@ -23,5 +27,12 @@ describe("My First Test suite", () => {
             cy.wrap($el).find("button").click()
           }
         })
+      // assert if logo text is correctly displayed
+      cy.get(".brand").should('have.text',"GREENKART")
+
+      // handle non cypress command the promise by manually  
+      cy.get(".brand").then(function(logoEle) {
+        cy.log(logoEle.text())
+      })
     })
   })
