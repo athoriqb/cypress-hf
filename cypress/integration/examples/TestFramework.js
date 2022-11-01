cce2c05818851866680bd36cee2121daa3ea47d0
athoriqb <athoriqb@gmail.com>
2022-11-01 18:36:54 +0700

    test

---
+++ cypress/integration/examples/TestFramework.js
@@ -0,0 +1,36 @@
+describe('My First Test Case', () => {
+
+  // first method
+
+  // let data
+
+  // before(() => {
+  //   cy.fixture("example").then((test) => {
+  //     data = test
+  //   })
+  // })
+
+  //   it('Visits the Kitchen Sink', () => {
+  //     cy.visit("https://rahulshettyacademy.com/angularpractice")
+  //     cy.get("input[name='name']:nth-child(2)").type(data.fullName)
+  //     cy.get("select").select(data.gender)
+  //   })
+
+  // second method
+  before(function() {
+    cy.fixture("example").then(function(data) {
+      this.data = data
+    })
+  })
+
+    it('Visits the Kitchen Sink', function () {
+      cy.visit("https://rahulshettyacademy.com/angularpractice")
+      cy.get("input[name='name']:nth-child(2)").type(this.data.fullName)
+      cy.get("select").select(this.data.gender)
+      cy.get(":nth-child(4) > .ng-untouched").should('have.value',this.data.fullName)
+      cy.get("input[name='name']:nth-child(2)").should('have.attr',"minlength","2")
+      cy.get("#inlineRadio3").should("be.disabled")
+      cy.get(":nth-child(2) > .nav-link").click()
+      cy.selectProduct("Blackberry")
+    })
+  })
